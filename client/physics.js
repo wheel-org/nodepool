0749fd9bbd8a24d94ddaace380ff2e2b7b98f397
Michael Kim <micklepickle98@gmail.com>
2017-09-17 01:08:51 -0400

    Basic table collision

--- client/physics.js
+++ client/physics.js
@@ -1,11 +1,11 @@
-var TIME_BETWEEN_FRAMES = 16.6666666;
+var TIME_BETWEEN_FRAMES = 1;
 var DELTA_TIME = 1000 / TIME_BETWEEN_FRAMES;
-var DRAG = 0.985;
+var DRAG = 0.995;
 var BALL_RADIUS = 2.8575;
 
 // 0 is White Ball
 var balls = [
-	ball(50, 55.5, "#FFF"),
+	ball(50, 51.5, "#FFF"),
 	ball(152.5, 51.5, "rgba(255, 0, 0, 1)"), // Front (1)
 
 	ball(158, 48, "rgba(255, 0, 0, 1)"), // Second Row
@@ -28,8 +28,24 @@ var balls = [
 ];
 
 var collidingWalls = [
-	line(0, 4, 0, 96),
-	line(0, 4, -10, -3),
+	line(0, 94, 0, 6),
+	line(0, 6, -10, -3),
+	line(-3, -10, 6, 0),
+	line(6, 0, 94, 0),
+	line(94, 0, 95, -6),
+	line(105, -6, 106, 0),
+	line(106, 0, 194, 0),
+	line(194, 0, 203, -10),
+	line(210, -3, 200, 6),
+	line(200, 6, 200, 94),
+	line(200, 94, 210, 103),
+	line(203, 110, 194, 100),
+	line(194, 100, 106, 100),
+	line(106, 100, 105, 106),
+	line(95, 106, 94, 100),
+	line(94, 100, 6, 100),
+	line(6, 100, -3, 110),
+	line(-10, 103, 0, 94)
 ];
 
 function line(x1, y1, x2, y2) { 
@@ -71,17 +87,41 @@ function length(a, b) {
 	return Math.sqrt(Math.pow(a.y - b.y, 2) + Math.pow(a.x - b.x, 2));
 }
 
-function isBallTouchingWall(ball, wall) { 
-	var l = length({ x: wall.x1, y: wall.y1 }, { x: wall.x2, y: wall.y2 });
-	var a = length({ x: ball.x, y: ball.y }, { x: wall.x2, y: wall.y2 });
-	var b = length({ x: wall.x1, y: wall.y1 }, { x: ball.x, y: ball.y });
-	return b * Math.sin(Math.acos(
-		(Math.pow(a, 2) - Math.pow(b, 2) - Math.pow(l, 2)) / (-2 * l * b))) <= ball.r;
+function isBallTouchingWall(ball, wall) {
+	var d = [wall.x2 - wall.x1, wall.y2 - wall.y1];
+	var f = [wall.x1 - ball.x + BALL_RADIUS, wall.y1 - ball.y + BALL_RADIUS];
+	var a = dotProduct(d, d);
+	var b = 2 * dotProduct(f, d);
+	var c = dotProduct(f, f) - BALL_RADIUS * BALL_RADIUS;
+	var dis = b*b - 4*a*c;
+
+	if (dis >= 0) {
+		dis = Math.sqrt(dis);
+		var t1 = (-b - dis) / (2*a);
+		var t2 = (-b + dis) / (2*a);
+
+		if (t1 >= 0 && t1 <= 1) {
+			return true;
+		}
+
+		if (t2 >= 0 && t2 <= 1) {
+			return true;
+		}
+
+		return false;
+	}
+	return false;
 }
 
 function simulateVelocityFrame(ball) { 
 	ball.x += (ball.dx * 100) / DELTA_TIME;	
 	ball.y += (ball.dy * 100) / DELTA_TIME;
+
+	if (ball.x < BALL_RADIUS) ball.x = BALL_RADIUS;
+	else if (ball.x > 200) ball.x = 200;
+
+	if (ball.y < BALL_RADIUS) ball.y = BALL_RADIUS;
+	else if (ball.y > 130 - BALL_RADIUS) ball.y = 130 - BALL_RADIUS;
 }
 
 function dotProduct(a, b) { 
@@ -93,7 +133,7 @@ function subVec(a, b) {
 }
 
 function updateBalls() { 
-	for (var i = 0; i < balls.length; i++) { 
+	for (var i = 0; i < balls.length; i++) {
 		simulateVelocityFrame(balls[i]);
 		balls[i].dx *= DRAG;
 		balls[i].dy *= DRAG;		
@@ -117,11 +157,16 @@ function updateBalls() {
 				b.dy = bNewY;
 				simulateVelocityFrame(balls[i]);
 				simulateVelocityFrame(balls[j]);
-			}	
+			}
 		}
-		for (var j = 0; j < collidingWalls.length; j++) { 
-			if (isBallTouchingWall(balls[i], collidingWalls[j])) { 
-				
+
+		for (var j = 0; j < collidingWalls.length; j++) {
+			if (isBallTouchingWall(balls[i], collidingWalls[j])) {
+				var lenx = collidingWalls[j].x2 - collidingWalls[j].x1;
+				var leny = collidingWalls[j].y2 - collidingWalls[j].y1;
+				var angle = Math.atan2(leny, lenx);
+				balls[i].dx *= -0.9;
+				balls[i].dy *= -0.9;
 			}
 		}
 	}
@@ -134,7 +179,7 @@ function update() {
 $(document).ready(function () {
 	$(document).click(function () { 
 
-		balls[0].dx += 2;
+		balls[0].dx += 10;
 	});
 });
 setInterval(update, TIME_BETWEEN_FRAMES);
